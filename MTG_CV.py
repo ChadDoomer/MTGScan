2b4843a9daeee559d5d9c79a4c1387e810f70c39
ChadDoomer <acarlisle@eagles.usi.edu>
2023-09-13 09:53:18 -0500

    Added a working version of the image comparison

--- MTG_CV.py
+++ MTG_CV.py
@@ -1,20 +1,41 @@
-import cv2
 import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
 
-img1 = cv2.imread('Test_Images/Scotch_1.jpg')
-img2 = cv2.imread('Test_Images/Scotch_12.jpg')
-img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-def mse(img1, img2):
-    h, w = img1.shape
-    diff = cv2.subtract(img1, img2)
-    err = np.sum(diff**2)
-    mse = err/(float(h*w))
-    return mse, diff
+img1 = cv.imread('Test_Images/Scotch_1.jpg', cv.IMREAD_GRAYSCALE)  # queryImage
+img2 = cv.imread('Test_Images/Scotch_12.jpg', cv.IMREAD_GRAYSCALE)  # trainImage
 
-error, diff = mse(img1, img2)
-print("Image matching Error between the two images:",error)
+#  convert images to 8bit
+image8bit1 = cv.normalize(img1, None, 0, 255, cv.NORM_MINMAX).astype('uint8')
+image8bit2 = cv.normalize(img2, None, 0, 255, cv.NORM_MINMAX).astype('uint8')
 
-cv2.imshow("difference", diff)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# Initiate SIFT detector
+sift = cv.SIFT.create()
+
+# find the keypoints and descriptors with SIFT
+kp1, des1 = sift.detectAndCompute(image8bit1, None)
+kp2, des2 = sift.detectAndCompute(image8bit2, None)
+
+# FLANN parameters
+FLANN_INDEX_KDTREE = 1
+index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+search_params = dict(checks=50)  # number of times the trees in the index should be recursively traversed
+flann = cv.FlannBasedMatcher(index_params, search_params)
+matches = flann.knnMatch(des1, des2, k=2)
+
+# Need to draw only good matches, so create a mask
+matchesMask = [[0, 0] for i in range(len(matches))]
+
+# ratio test
+for i, (m, n) in enumerate(matches):
+    if m.distance < 0.7 * n.distance:
+        matchesMask[i] = [1, 0]
+
+draw_params = dict(matchColor=(0, 255, 0),
+                   singlePointColor=(255, 0, 0),
+                   matchesMask=matchesMask,
+                   flags=cv.DrawMatchesFlags_DEFAULT)
+
+img3 = cv.drawMatchesKnn(image8bit1, kp1, image8bit2, kp2, matches, None, **draw_params)
+print(matchesMask)
+plt.imshow(img3, ), plt.show()
